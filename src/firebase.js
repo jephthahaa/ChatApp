907cf14a50b417f5245cdf2d33ffc4ce7391bd1f
Jephthah05 <jephthahaansah@gmail.com>
2023-07-31 23:30:12 +0000

    updated codes

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
+yaya// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from "firebase/firestore";
+import { getDatabase, ref, onValue, set, onDisconnect } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNAx3reEBJIPL07-s5PHYbAngZb2ULr_Q",
@@ -16,7 +18,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
-const db = getFirestore();
+const db = getFirestore(app);
+const database = getDatabase(app); // Add this line for Firebase Realtime Database
 
-export { app, auth, storage, db };
+export { app, auth, storage, db, database, ref, onValue, set, onDisconnect }; // Export all necessary functions
+
 
